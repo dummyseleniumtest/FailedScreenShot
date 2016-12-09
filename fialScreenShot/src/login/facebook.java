9b6639eee47449cedcc78ff1fd0fb8c8e7b59eb9
naveen <naveensharma028@yahoo.com>
2016-12-09 21:54:34 +0530

    changed happen

--- fialScreenShot/src/login/facebook.java
+++ fialScreenShot/src/login/facebook.java
@@ -36,7 +36,7 @@ public class facebook {
 	  driver.navigate().to("https://www.facebook.com/");
 	  logger.info("App Launched");
 	  driver.manage().window().maximize();
-	  logger.info("Window Maximized");
+	  logger.info("Window is Maximized");
 	  driver.findElement(By.xpath("//*[@id='emailsssss']")).sendKeys("facebook login");
 	  
 	  logger.info("Enter UserName");
